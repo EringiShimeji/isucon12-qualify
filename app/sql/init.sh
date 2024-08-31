716499dd3af4f71106e77cc7afda3ed2c96d2031
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-08-31 22:50:21 +0900

    Revert "init.shで全てのsqlを読ませる"
    This reverts commit a2bfbbd2161e9a84feae58ff7e8c0a39a73d2e70.

--- app/sql/init.sh
+++ app/sql/init.sh
@@ -9,14 +9,7 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
-
 # MySQLを初期化
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < admin/01_create_mysql_database.sql
-
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
