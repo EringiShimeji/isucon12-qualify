3678e2fd9d7523fe6822bbe9fc2e17d7136ccbf6
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-08-31 22:49:54 +0900

    databaseがないよ！

--- app/sql/init.sh
+++ app/sql/init.sh
@@ -15,7 +15,7 @@ mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < admin/01_create_mysql_database.sql
+		< admin/01_create_mysql_database.sql
 
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
