3cda003e13496efb34379de452d7ab50f3ddf0a7
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-08-31 22:10:53 +0900

    10_schema.sqlを読み込むようにする

--- app/sql/init.sql
+++ app/sql/init.sql
@@ -3,4 +3,3 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
-CREATE INDEX `tenant_id_competition_id_idx` ON visit_history (`tenant_id`, `competition_id`)
